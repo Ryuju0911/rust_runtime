44438567f866e977f2edde6143ff38cc1a8602f7
Ryuju0911 <pinebook0911@gmail.com>
2023-09-08 18:34:01 +0900

    Change tab size from 2 spaces to 4 spaces.

--- src/container/container.rs
+++ src/container/container.rs
@@ -1,41 +1,40 @@
-use std::{path::PathBuf, fs};
+use std::{fs, path::PathBuf};
 
 use anyhow::Result;
 
 use crate::container::{ContainerStatus, State};
 
-
 #[derive(Debug)]
 pub struct Container {
-  pub state: State,
-  pub root: PathBuf
+    pub state: State,
+    pub root: PathBuf,
 }
 
 impl Container {
-  pub fn new(
-    container_id: &str,
-    status: ContainerStatus,
-    pid: Option<i32>,
-    bundle: &str,
-    container_root: &PathBuf,
-  ) -> Result<Self> {
-    let container_root = fs::canonicalize(container_root)?;
-    let state = State::new(container_id, status, pid, bundle);
-    Ok(Self {
-      state,
-      root: container_root,
-    })
-  }
+    pub fn new(
+        container_id: &str,
+        status: ContainerStatus,
+        pid: Option<i32>,
+        bundle: &str,
+        container_root: &PathBuf,
+    ) -> Result<Self> {
+        let container_root = fs::canonicalize(container_root)?;
+        let state = State::new(container_id, status, pid, bundle);
+        Ok(Self {
+            state,
+            root: container_root,
+        })
+    }
 
-  pub fn id(&self) -> &str {
-    self.state.id.as_str()
-  }
+    pub fn id(&self) -> &str {
+        self.state.id.as_str()
+    }
 
-  pub fn status(&self) -> ContainerStatus {
-    self.state.status
-  }
+    pub fn status(&self) -> ContainerStatus {
+        self.state.status
+    }
 
-  pub fn save(&self) -> Result<()> {
-    self.state.save(&self.root)
-  }
+    pub fn save(&self) -> Result<()> {
+        self.state.save(&self.root)
+    }
 }
--- src/container/state.rs
+++ src/container/state.rs
@@ -1,4 +1,8 @@
-use std::{collections::HashMap, path::PathBuf, fs::{self, File}};
+use std::{
+    collections::HashMap,
+    fs::{self, File},
+    path::PathBuf,
+};
 
 use anyhow::Result;
 use serde::{Deserialize, Serialize};
@@ -8,69 +12,69 @@ const STATE_FILE_PATH: &str = "state.json";
 #[derive(Serialize, Deserialize, Debug, Copy, Clone)]
 #[serde(rename_all = "camelCase")]
 pub enum ContainerStatus {
-  // StateCreating indicates that the container is being created
-  Creating,
-  // StateCreated indicates that the runtime has finished the create operation
-  Created,
-  // StateRunning indicates that the container process has executed the
-  // user-specified program but has not exited
-  Running,
-  // StateStopped indicates that the container process has exited
-  Stopped,
+    // StateCreating indicates that the container is being created
+    Creating,
+    // StateCreated indicates that the runtime has finished the create operation
+    Created,
+    // StateRunning indicates that the container process has executed the
+    // user-specified program but has not exited
+    Running,
+    // StateStopped indicates that the container process has exited
+    Stopped,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 #[serde(rename_all = "camelCase")]
 pub struct State {
-  // Version is the version of the specification that is supported.
-  pub oci_version: String,
-  // ID is the container ID
-  pub id: String,
-  // Status is the runtime status of the container.
-  pub status: ContainerStatus,
-  // Pid is the process ID for the container process.
-  pub pid: Option<i32>,
-  // Bundle is the path to the container's bundle directory.
-  pub bundle: String,
-  // Annotations are key values associated with the container.
-  pub annotations: HashMap<String, String>,
+    // Version is the version of the specification that is supported.
+    pub oci_version: String,
+    // ID is the container ID
+    pub id: String,
+    // Status is the runtime status of the container.
+    pub status: ContainerStatus,
+    // Pid is the process ID for the container process.
+    pub pid: Option<i32>,
+    // Bundle is the path to the container's bundle directory.
+    pub bundle: String,
+    // Annotations are key values associated with the container.
+    pub annotations: HashMap<String, String>,
 }
 
 impl State {
-  pub fn new(
-    container_id: &str,
-    status: ContainerStatus,
-    pid: Option<i32>,
-    bundle: &str,
-  ) -> Self {
-    Self {
-      oci_version: "v1.0.0".to_string(),
-      id: container_id.to_string(),
-      status,
-      pid,
-      bundle: bundle.to_string(),
-      annotations: HashMap::default(),
+    pub fn new(
+        container_id: &str,
+        status: ContainerStatus,
+        pid: Option<i32>,
+        bundle: &str,
+    ) -> Self {
+        Self {
+            oci_version: "v1.0.0".to_string(),
+            id: container_id.to_string(),
+            status,
+            pid,
+            bundle: bundle.to_string(),
+            annotations: HashMap::default(),
+        }
     }
-  }
 
-  pub fn save(&self, container_root: &PathBuf) -> Result<()> {
-    let state_file_path = container_root.join(STATE_FILE_PATH);
-    let file = fs::OpenOptions::new()
-      .read(true)
-      .write(true)
-      .append(false)
-      .create(true)
-      .truncate(true)
-      .open(state_file_path)
-      .expect("Unable to open");
-    serde_json::to_writer(&file, self)?;
-    Ok(())
-  }
+    pub fn save(&self, container_root: &PathBuf) -> Result<()> {
+        let state_file_path = container_root.join(STATE_FILE_PATH);
+        let file = fs::OpenOptions::new()
+            .read(true)
+            .write(true)
+            .append(false)
+            .create(true)
+            .truncate(true)
+            .open(state_file_path)
+            .expect("Unable to open");
+        serde_json::to_writer(&file, self)?;
+        Ok(())
+    }
 
-  pub fn load(container_root: &PathBuf) -> Result<Self> {
-    let state_file_path = container_root.join(STATE_FILE_PATH);
-    let file = File::open(state_file_path)?;
-    let state: Self = serde_json::from_reader(&file)?;
-    Ok(state)
-  }
+    pub fn load(container_root: &PathBuf) -> Result<Self> {
+        let state_file_path = container_root.join(STATE_FILE_PATH);
+        let file = File::open(state_file_path)?;
+        let state: Self = serde_json::from_reader(&file)?;
+        Ok(state)
+    }
 }
